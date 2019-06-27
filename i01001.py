a7d6942a15e81ed59042c2ab95a65b64d7495b16
wuyang-pc <wuyangv@gmail.com>
2019-06-28 00:30:10 +0800

    add

--- i01001.py
+++ i01001.py
@@ -1,4 +1,17 @@
+from fractions import Fraction
+from decimal import Decimal
 a = 2**2048
 print a
 
 print "这是中文"
+
+b = Fraction('19/155')
+d = Decimal('1.5')
+
+a = 2.0
+
+c = a*b
+dd = float(d)
+f = a * dd
+print c
+print dd
