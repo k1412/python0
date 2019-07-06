b9de4751e306cc4dadab4660e31171ad4e0dcf94
wuyang-pc <wuyangv@gmail.com>
2019-07-06 22:45:06 +0800

    add

---
+++ i11002.py
@@ -0,0 +1,6 @@
+u#读取文件练习
+
+f = open("aa.sd","r+")
+str1 = f.read(1000)
+f.close()
+print str1                                                                                                                                                                                                                                                                                                                                                                                                                                                          
